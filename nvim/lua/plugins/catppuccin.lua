494f36d716bf94860f3288ddf81c617f6f20438b
Koczka Dávid <koczkadavid@gmail.com>
2024-01-22 09:38:55 +0100

    Filter images from telescope find-file

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,36 +1,36 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      background = {
-        light = "macchiato",
-        dark = "macchiato",
-      },
-      transparent_background = true,
-      integrations = {
-        alpha = true,
-        cmp = true,
-        gitsigns = true,
-        indent_blankline = {
-          enabled = true,
-          scope_color = "lavender",
-          colored_indent_levels = false,
-        },
-        lsp_saga = true,
-        mason = true,
-        noice = true,
-        notify = true,
-        nvimtree = true,
-        telescope = {
-          enabled = true,
-        },
-        treesitter = true,
-      },
-    })
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			background = {
+				light = "macchiato",
+				dark = "macchiato",
+			},
+			transparent_background = true,
+			integrations = {
+				alpha = true,
+				cmp = true,
+				gitsigns = true,
+				indent_blankline = {
+					enabled = true,
+					scope_color = "lavender",
+					colored_indent_levels = false,
+				},
+				lsp_saga = true,
+				mason = true,
+				noice = true,
+				notify = true,
+				nvimtree = true,
+				telescope = {
+					enabled = true,
+				},
+				treesitter = true,
+			},
+		})
 
-    vim.o.termguicolors = true
-    vim.cmd.colorscheme("catppuccin")
-  end,
+		vim.o.termguicolors = true
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,35 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
-  config = function()
-    local treesitter = require("nvim-treesitter.configs")
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
+		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+		parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
 
-    treesitter.setup({
-      highlight = { enable = true },
-      indent = { enable = true },
-      autotag = { enable = true },
-      auto_install = true,
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "markdown",
-        "markdown_inline",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-      },
-    })
-  end,
+		treesitter.setup({
+			highlight = { enable = true },
+			indent = { enable = true },
+			autotag = { enable = true },
+			auto_install = true,
+			ensure_installed = {
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"html",
+				"css",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"lua",
+				"vim",
+				"dockerfile",
+				"gitignore",
+			},
+		})
+	end,
 }
