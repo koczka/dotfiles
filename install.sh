c536f7dc895614e090f3fa741e571a1b5278ca70
Koczka Dávid <koczkadavid@gmail.com>
2024-01-25 10:12:48 +0100

    Linked everforest color scheme to catppuccin status line in tmux

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -s ~/.dotfiles/nvim ~/.config
 ln -s ~/.dotfiles/alacritty ~/.config
 ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc 
+ln -s ~/.dotfiles/tmux/catppuccin-everforest.tmuxtheme ~/.tmux/plugins/tmux/catppuccin-everforest.tmuxtheme  
