d19b80cc61d300c768e7d406f30a39b1723d25d8
Koczka Dávid <koczkadavid@gmail.com>
2024-02-04 11:28:03 +0100

    Customized lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,15 +3,19 @@ return {
 	config = function()
 		local git_blame = require("gitblame")
 		local lualine = require("lualine")
+		--
 
 		lualine.setup({
-			-- options = {
-			-- 	theme = "catppuccin",
-			-- },
+			options = {
+				-- theme = "catppuccin",
+				section_separators = { left = " ", right = " " },
+				component_separators = { left = "|", right = "|" },
+			},
 			sections = {
 				lualine_c = {
 					{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
 				},
+				lualine_x = { "filename", "filetype" },
 			},
 		})
 	end,
