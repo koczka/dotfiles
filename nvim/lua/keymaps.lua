b325cd0d4229dc18e1ce2d3716978d96268fce9f
Koczka Dávid <koczkadavid@gmail.com>
2024-02-23 22:04:57 +0100

    Disabled arrows for insert mode

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -8,6 +8,10 @@ keymap.set("n", "<Up>", "<nop>")
 keymap.set("n", "<Down>", "<nop>")
 keymap.set("n", "<Left>", "<nop>")
 keymap.set("n", "<Right>", "<nop>")
+keymap.set("i", "<Up>", "<nop>")
+keymap.set("i", "<Down>", "<nop>")
+keymap.set("i", "<Left>", "<nop>")
+keymap.set("i", "<Right>", "<nop>")
 
 -- clear search highlights
 keymap.set("n", "<leader>cs", ":nohl<CR>")
@@ -20,15 +24,15 @@ keymap.set("n", "<leader>+", "<C-a>") -- increment
 keymap.set("n", "<leader>-", "<C-x>") -- decrement
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v")                                        -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s")                                        -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=")                                        -- make split windows equal width & height
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
 keymap.set("n", "<leader>sx", ":close<CR>", { noremap = true, silent = true }) -- close current split window
 
-keymap.set("n", "<leader>to", ":tabnew<CR>")                                   -- open new tab
-keymap.set("n", "<leader>tx", ":tabclose<CR>")                                 -- close current tab
-keymap.set("n", "<leader>tn", ":tabn<CR>")                                     --  go to next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>")                                     --  go to previous tab
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
 -- horizontal navigation
 keymap.set("n", "<C-d>", "<C-d>zz")
