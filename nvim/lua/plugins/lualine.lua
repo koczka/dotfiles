9a3139183c3d3a8bb7b3e7b31668e3bdf516257b
Koczka Dávid <koczkadavid@gmail.com>
2024-03-04 08:15:40 +0100

    Removed active lsp names from lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -17,35 +17,38 @@ return {
 					{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
 				},
 				lualine_x = {
-					{
-						function()
-							local lsps = vim.lsp.get_active_clients({ bufnr = vim.fn.bufnr() })
-							local icon = require("nvim-web-devicons").get_icon_by_filetype(
-								vim.api.nvim_buf_get_option(0, "filetype")
-							)
-							if lsps and #lsps > 0 then
-								local names = {}
-								for _, lsp in ipairs(lsps) do
-									table.insert(names, lsp.name)
-								end
-								return string.format("%s %s", table.concat(names, ", "), icon or "")
-							else
-								return icon or ""
-							end
-						end,
-						on_click = function()
-							vim.api.nvim_command("LspInfo")
-						end,
-						color = function()
-							local _, color = require("nvim-web-devicons").get_icon_cterm_color_by_filetype(
-								vim.api.nvim_buf_get_option(0, "filetype")
-							)
-							return { fg = color }
-						end,
-					},
 					"encoding",
 				},
 			},
 		})
 	end,
 }
+
+
+
+				-- 	{
+				-- 		function()
+				-- 			local lsps = vim.lsp.get_active_clients({ bufnr = vim.fn.bufnr() })
+				-- 			local icon = require("nvim-web-devicons").get_icon_by_filetype(
+				-- 				vim.api.nvim_buf_get_option(0, "filetype")
+				-- 			)
+				-- 			if lsps and #lsps > 0 then
+				-- 				local names = {}
+				-- 				for _, lsp in ipairs(lsps) do
+				-- 					table.insert(names, lsp.name)
+				-- 				end
+				-- 				return string.format("%s %s", table.concat(names, ", "), icon or "")
+				-- 			else
+				-- 				return icon or ""
+				-- 			end
+				-- 		end,
+				-- 		on_click = function()
+				-- 			vim.api.nvim_command("LspInfo")
+				-- 		end,
+				-- 		color = function()
+				-- 			local _, color = require("nvim-web-devicons").get_icon_cterm_color_by_filetype(
+				-- 				vim.api.nvim_buf_get_option(0, "filetype")
+				-- 			)
+				-- 			return { fg = color }
+				-- 		end,
+				-- 	},
