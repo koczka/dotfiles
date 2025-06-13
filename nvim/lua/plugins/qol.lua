c167d904b4b202c72ed71c472822cdbd141005fa
Koczka Dávid <koczkadavid@gmail.com>
2025-06-13 16:50:50 +0200

    Remove deprecated/unnecessary plugins

--- nvim/lua/plugins/qol.lua
+++ nvim/lua/plugins/qol.lua
@@ -1,31 +1,11 @@
 return {
 	-- quality of life plugins
 	{ "nvim-lua/plenary.nvim" },
-	{ "vim-scripts/ReplaceWithRegister" }, -- replace with register contents using motion (gr + motion)
+	{
+		"vim-scripts/ReplaceWithRegister",
+		config = function()
+			vim.api.nvim_set_keymap("n", "gr", '"+gr', { noremap = false, silent = true })
+		end,
+	},
 	{ "christoomey/vim-tmux-navigator" }, -- tmux & split window navigation
-	{
-		"brenoprata10/nvim-highlight-colors",
-		opts = {
-			render = "virtual",
-			virtual_symbol = "■",
-			virtual_symbol_prefix = " ",
-		},
-	},
-	{
-		"xiyaowong/transparent.nvim",
-		opts = {
-			extra_groups = {
-				"NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
-				"NvimTreeNormal", -- NvimTree
-				"NvimTreeEndOfBuffer",
-			},
-		},
-	},
-	{
-		"tris203/precognition.nvim",
-		config = {
-			vim.keymap.set("n", "<leader>mp", ":Precognition peek<CR>", { noremap = true, silent = true }),
-			startVisible = false,
-		},
-	},
 }
