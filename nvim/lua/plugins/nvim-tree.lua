01e25537517a3a83f2a3071e8a216e4718bffbb2
Koczka Dávid <koczkadavid@gmail.com>
2025-06-13 16:49:09 +0200

    Remove web-devicons in favour to mini icons

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,6 +1,5 @@
 return {
 	"nvim-tree/nvim-tree.lua",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		-- recommended settings from nvim-tree documentation
 		vim.g.loaded = 1
