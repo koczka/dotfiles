b57a5c1e0e1b1b4f15dedc39e553fcc776f083f2
Koczka Dávid <koczkadavid@gmail.com>
2025-08-29 11:11:43 +0200

    Removed ReplaceWithRegister plugin

--- nvim/lua/plugins/qol.lua
+++ nvim/lua/plugins/qol.lua
@@ -1,11 +1,4 @@
 return {
-	-- quality of life plugins
 	{ "nvim-lua/plenary.nvim" },
-	{
-		"vim-scripts/ReplaceWithRegister",
-		config = function()
-			vim.api.nvim_set_keymap("n", "gr", '"+gr', { noremap = false, silent = true })
-		end,
-	},
 	{ "christoomey/vim-tmux-navigator" }, -- tmux & split window navigation
 }
