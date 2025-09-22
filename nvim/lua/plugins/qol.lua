099a3fa9dd7c8b4e23db9aa64da6314346bafc1e
Koczka Dávid <koczkadavid@gmail.com>
2025-09-22 19:18:12 +0200

    Add split maximizer

--- nvim/lua/plugins/qol.lua
+++ nvim/lua/plugins/qol.lua
@@ -23,4 +23,10 @@ return {
 			},
 		},
 	},
+	{
+		"szw/vim-maximizer", -- maximizes and restores current window
+		config = function()
+			vim.keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
+		end,
+	},
 }
