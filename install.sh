c2ab719534b31d8e38459255057d76ad0802def9
Koczka Dávid <koczkadavid@gmail.com>
2025-09-22 19:23:03 +0200

    Removed custom theme file from install script

--- install.sh
+++ install.sh
@@ -5,4 +5,3 @@ ln -s ~/.dotfiles/alacritty ~/.config
 ln -s ~/.dotfiles/sketchybar/ ~/.config
 ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc 
-ln -s ~/.dotfiles/tmux/catppuccin-everforest.tmuxtheme ~/.tmux/plugins/tmux/catppuccin-everforest.tmuxtheme  
